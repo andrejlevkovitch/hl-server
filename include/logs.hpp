efa0bfa18faa5e789035f516cea8ad17545b19ea
andrejlevkovitch <andrejlevkovitch@gmail.com>
2020-06-05 12:36:10 +0300

    change logs for release version

--- include/logs.hpp
+++ include/logs.hpp
@@ -3,7 +3,18 @@
 #include <chrono>
 
 // XXX we don't need log time, so redefine this macro for avoid system call
-const std::chrono::time_point<std::chrono::system_clock> t_placeholder{};
-#define GET_LOG_TIME() t_placeholder
+const std::chrono::time_point<std::chrono::system_clock> time_log_placeholder{};
+#define GET_LOG_TIME() time_log_placeholder
+
+#ifdef NDEBUG
+#  define LOG_DEBUG(...)
+#  define LOG_WARNING(...)
+
+#  define LOG_THROW(ExceptionType, ...)                                        \
+    {                                                                          \
+      boost::format fmt = logs::messageHandler(__VA_ARGS__);                   \
+      throw ExceptionType{fmt.str()};                                          \
+    }
+#endif
 
 #include <simple_logs/logs.hpp>
