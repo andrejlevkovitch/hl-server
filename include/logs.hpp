78f5a62ea7748151dc26c0a269877ceba87494b0
andrejlevkovitch <andrejlevkovitch@gmail.com>
2020-09-19 16:40:00 +0300

    change logs

--- include/logs.hpp
+++ include/logs.hpp
@@ -6,15 +6,57 @@
 const std::chrono::time_point<std::chrono::system_clock> time_log_placeholder{};
 #define GET_LOG_TIME() time_log_placeholder
 
-#ifdef NDEBUG
-#  define LOG_DEBUG(...)
-#  define LOG_WARNING(...)
-
-#  define LOG_THROW(ExceptionType, ...)                                        \
-    {                                                                          \
-      boost::format fmt = logs::messageHandler(__VA_ARGS__);                   \
-      throw ExceptionType{fmt.str()};                                          \
-    }
-#endif
-
 #include <simple_logs/logs.hpp>
+
+namespace logs {
+class LogLevel final {
+public:
+  static void setMin(logs::Severity min) {
+    LogLevel::minLogLevel() = min;
+  }
+
+  static logs::Severity getMin() {
+    return LogLevel::minLogLevel();
+  }
+
+private:
+  static logs::Severity &minLogLevel() {
+    static logs::Severity retval;
+    return retval;
+  }
+};
+
+class CurrentLogger final : public BasicLogger {
+public:
+  static CurrentLogger &get() {
+    static CurrentLogger retval;
+    return retval;
+  }
+
+  void log(Severity                                           severity,
+           std::string_view                                   fileName,
+           int                                                lineNumber,
+           std::string_view                                   functionName,
+           std::chrono::time_point<std::chrono::system_clock> timePoint,
+           std::thread::id                                    threadId,
+           boost::format message) noexcept override {
+    if (severity >= LogLevel::getMin()) {
+      std::cerr << getRecord(severity,
+                             fileName,
+                             lineNumber,
+                             functionName,
+                             timePoint,
+                             threadId,
+                             std::move(message))
+                << std::endl;
+    }
+
+    if (Severity::Failure == severity) {
+      exit(EXIT_FAILURE);
+    }
+  }
+};
+} // namespace logs
+
+#undef LOGGER
+#define LOGGER logs::CurrentLogger::get()
